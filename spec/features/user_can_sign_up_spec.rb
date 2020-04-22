f18c2c585451454b82d89e57842606f6f9d94185
Marcus.Sjoqvist <marcus.sjoqvist@viametrics.com>
2020-04-22 13:39:03 +0200

    Adding missing devise parameter sanitizer to appcontroller, and adding some sad path tests to sign-up form

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -15,8 +15,39 @@ feature 'user can sign up' do
         fill_in "Password", with: "qwerty123"
         fill_in "Password confirmation", with: "qwerty123"
         click_on "Create"
-        expect(page).to have_content "User signed up successfully"
+        expect(page).to have_content "You have signed up successfully"
     end
 
+    it 'should give an error no name is given' do
+        fill_in "Email", with: "bob@bobert.com"
+        fill_in "Password", with: "qwerty123"
+        fill_in "Password confirmation", with: "qwerty123"
+        click_on "Create"
+        expect(page).to have_content "Name can't be blank"
+    end
 
+    it 'should give an error if passwords dont match' do
+        fill_in "Name", with: "Bobby"
+        fill_in "Email", with: "bob@bobert.com"
+        fill_in "Password", with: "qwerty123"
+        fill_in "Password confirmation", with: "qwerty124"
+        click_on "Create"
+        expect(page).to have_content "Password confirmation doesn't match Password"
+    end
+
+    it 'does not allow duplicate signups' do
+        fill_in "Name", with: "Bobert"
+        fill_in "Email", with: "bob@bobert.com"
+        fill_in "Password", with: "qwerty123"
+        fill_in "Password confirmation", with: "qwerty123"
+        click_on "Create"
+        click_on "Logout"
+        click_on "Sign up"
+        fill_in "Name", with: "Bobert"
+        fill_in "Email", with: "bob@bobert.com"
+        fill_in "Password", with: "qwerty123"
+        fill_in "Password confirmation", with: "qwerty123"
+        click_on "Create"
+        expect(page).to have_content "Name has already been taken"
+    end 
 end
