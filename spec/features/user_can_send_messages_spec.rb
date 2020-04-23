bbf07759d92606878fa990b6972146bd50cbcb1f
pauline_barnades <barnades.pauline@gmail.com>
2020-04-23 13:56:10 +0200

    Edit test to send message spec

--- spec/features/user_can_send_messages_spec.rb
+++ spec/features/user_can_send_messages_spec.rb
@@ -1,12 +1,26 @@
-features "user can send messages" do 
+feature "user can send messages" do 
     let(:sender) {create(:user, email: 'sender@mail.com', password:'wderfhujikki', name:'pauline') }
-    let(:receiver) {create(:user, email: 'sender@mail.com',password:' hdjkkkklkk' name:'ali') }
-     describe
+    let(:receiver) {create(:user, email: 'receiver@mail.com',password:' hdjkkkklkk', name:'ali') }
+     describe "sender logs-in and sends message" do 
      
      before do
-          login_as(user, scope: :user)
+        
+          login_as(sender, scope: :user)
           visit root_path
+          click_on "Inbox"
+          click_on "Compose"
+          select "ali", from: "Recipients"
+          fill_in "Subject", with: "Bonjour"
+          fill_in "Message", with: "Blablalal"
+          click_on "Send Message"
+          
         end
+        
+        it "receives success message" do 
+          expect(page).to have_content "Your message was successfully sent!"
+        
+        end
+    end
 
 
 end
