4b79b5a46cb12b5d1bb1c51cd17031d6532dccf8
allezz <aerbay@gmail.com>
2020-04-24 12:07:28 +0200

    Add conversation path

--- spec/features/user_can_trash_messages_spec.rb
+++ spec/features/user_can_trash_messages_spec.rb
@@ -3,19 +3,16 @@ feature "User can trash messages" do
     let(:receiver) {create(:user, email: "receiver@mail.com",password:"hdjkkkklkk", name:"ali") }
 
     describe "sender sends message and receivers logs in and deletes it" do  
-
         before do
-            sender.send_message(receiver, "Lorem ipsum...", "Subject")
+            abc =sender.send_message(receiver, "Lorem ipsum...", "Subject")
+            puts abc
             login_as(receiver, scope: :user)
-            visit root_path
-            click_on "Inbox"
-            click_on "View"
+            visit conversation_path(abc.notification_id)
             click_on "Move to trash"        
         end
         
         it "receiver checks inbox" do
-            expect(page).to_not have_content "Are you sure?"
+            expect(page).to_not have_content "Lorem ipsum..."
         end
-
     end
 end 
